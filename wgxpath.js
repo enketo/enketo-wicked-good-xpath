2770b2d0f199bf410be1e2db2ea370a8bf9200b2
gdennis@google.com <gdennis@google.com@be6b7ece-51eb-82be-2a6a-b935ba6741f4>
2012-09-20 19:21:52 +0000

    exporting after internally merging changes from r11
    git-svn-id: http://wicked-good-xpath.googlecode.com/svn/trunk@12 be6b7ece-51eb-82be-2a6a-b935ba6741f4

--- wgxpath.js
+++ wgxpath.js
@@ -104,7 +104,7 @@ wgxpath.XPathResult_ = function(value, type) {
       type != wgxpath.XPathResultType_.NUMBER_TYPE &&
       type != wgxpath.XPathResultType_.BOOLEAN_TYPE &&
       !(value instanceof wgxpath.NodeSet)) {
-    throw Error('document.evaluate called with wrong result type.');
+    throw Error('value could not be converted to the specified type');
   }
   this['resultType'] = type;
   var nodes;
@@ -148,14 +148,14 @@ wgxpath.XPathResult_ = function(value, type) {
   this['iterateNext'] = function() {
     if (type != wgxpath.XPathResultType_.UNORDERED_NODE_ITERATOR_TYPE &&
         type != wgxpath.XPathResultType_.ORDERED_NODE_ITERATOR_TYPE) {
-      throw Error('iterateNext called with wrong result type.');
+      throw Error('iterateNext called with wrong result type');
     }
     return (index >= nodes.length) ? null : nodes[index++];
   };
   this['snapshotItem'] = function(i) {
     if (type != wgxpath.XPathResultType_.UNORDERED_NODE_SNAPSHOT_TYPE &&
         type != wgxpath.XPathResultType_.ORDERED_NODE_SNAPSHOT_TYPE) {
-      throw Error('snapshotItem called with wrong result type.');
+      throw Error('snapshotItem called with wrong result type');
     }
     return (i >= nodes.length || i < 0) ? null : nodes[i];
   };
